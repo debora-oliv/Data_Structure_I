72a24fa97d14e4da7dc5c5e791e850a72e21371e
Débora Alves <deboraalves007@hotmail.com>
2025-10-31 12:21:14 -0300

    fix: método efetuarAumento

--- exercicios/exercicio_1/Funcionario.java
+++ exercicios/exercicio_1/Funcionario.java
@@ -1,7 +1,5 @@
 package exercicio_1;
 
-import java.util.ArrayList;
-
 public class Funcionario {
 
     private static int nextId = 0;
@@ -12,8 +10,6 @@ public class Funcionario {
 
     private String nome;
 
-    private static final ArrayList<Funcionario> funcionarios = new ArrayList<>();
-
     public Funcionario(Double salario, String nome) {
         id = ++nextId;
 
@@ -21,26 +17,7 @@ public class Funcionario {
 
         if (salario >= 1500 && salario < 12000)
             this.salario = salario;
-        else throw new IllegalArgumentException("Salário inválido");
-
-        funcionarios.add(this);
-    }
-
-    public static void listarFuncionarios() {
-        for (Funcionario f : funcionarios) {
-            System.out.println(f);
-        }
-    }
-
-    public static void efetuarAumento(Integer id, Double porcentagem) {
-        for (Funcionario funcionario : funcionarios) {
-            if (funcionario.getId() == id) {
-                funcionario.salario += funcionario.getSalario() * (porcentagem / 100);
-                System.out.println("Aumento efetuado com sucesso!");
-                return;
-            }
-        }
-        throw new IllegalArgumentException("Esse ID não existe");
+        else throw new IllegalArgumentException("Salário deve estar entre 1500 e 12000");
     }
 
     @Override
@@ -54,6 +31,13 @@ public class Funcionario {
 
     public Double getSalario() {
         return salario;
+    }
+
+    public void setSalary(Double salario) {
+        if(salario > 1500 && salario <= 12000)
+            this.salario = salario;
+        else
+            throw new IllegalArgumentException("Salário deve estar entre 1500 e 12000");
     }
 
     public String getNome() {
--- exercicios/exercicio_1/Programa.java
+++ exercicios/exercicio_1/Programa.java
@@ -1,5 +1,7 @@
 package exercicio_1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Programa {
@@ -8,6 +10,7 @@ public class Programa {
 
         System.out.print("Quantos funcionários serão registrados? ");
         var n = input.nextInt();
+        List<Funcionario> funcionarios = new ArrayList<>(n);
 
         for (int i = 1; i <= n; i++) {
             System.out.printf("Funcionário #%s", i + "\n");
@@ -16,8 +19,7 @@ public class Programa {
             System.out.print("Salário: ");
             var salario = input.nextDouble();
 
-            Funcionario f = new Funcionario(salario, nome);
-            System.out.println(f + "\n");
+            funcionarios.add(new Funcionario(salario, nome));
         }
 
         System.out.print("ID do funcionário que receberá o aumento: ");
@@ -26,10 +28,27 @@ public class Programa {
         System.out.print("Porcentagem do aumento: ");
         var aumento = input.nextDouble();
 
-        Funcionario.efetuarAumento(id, aumento);
+        efetuarAumento(funcionarios, id, aumento);
 
         System.out.print('\n');
         System.out.println("Lista de funcionários:");
-        Funcionario.listarFuncionarios();
+        listarFuncionarios(funcionarios);
+    }
+
+    public static void listarFuncionarios(List<Funcionario> funcionarios) {
+        for (Funcionario f : funcionarios) {
+            System.out.println(f);
+        }
+    }
+
+    public static void efetuarAumento(List<Funcionario> funcionarios, Integer id, Double porcentagem) {
+        for (Funcionario funcionario : funcionarios) {
+            if (funcionario.getId() == id) {
+                funcionario.setSalary(funcionario.getSalario() + funcionario.getSalario() * (porcentagem / 100));
+                System.out.println("Aumento efetuado com sucesso!");
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Esse ID não existe");
     }
 }
